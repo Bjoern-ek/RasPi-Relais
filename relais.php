493eff93d62dbff3510e8672b1c75628b1bfc882
Bjoern-ek <bjoern.eisenkruste@arcor.de>
2013-07-19 11:56:27 +0200

    neue Bezeichnung

--- relais.php
+++ relais.php
@@ -13,20 +13,20 @@
  <body>
 
 <form action="relais.php" method ="post">
-   <input type="checkbox" name="LED1" value="2">SolarPumpe<br> 
-   <input type="checkbox" name="LED2" value="2">LED 2<br> 
-   <input type="checkbox" name="LED3" value="2">LED 3<br> 
-   <input type="checkbox" name="LED4" value="2">LED 4<br> 
-   <input type="checkbox" name="LED5" value="2">LED 5<br> 
-   <input type="checkbox" name="LED6" value="2">LED 6<br> 
-   <input type="checkbox" name="LED7" value="2">LED 7<br> 
-   <input type="checkbox" name="LED8" value="2">LED 8<br> 
+   <input type="checkbox" name="IO1" value="2">SolarPumpe<br> 
+   <input type="checkbox" name="IO2" value="2">SolarVentil<br> 
+   <input type="checkbox" name="IO3" value="2">HeizungsPumpe<br> 
+   <input type="checkbox" name="IO4" value="2">KesselPumpe<br> 
+   <input type="checkbox" name="IO5" value="2">WandHzPumpe<br> 
+   <input type="checkbox" name="IO6" value="2">Ventil WW<br> 
+   <input type="checkbox" name="IO7" value="2">Ventil Heizung<br> 
+   <input type="checkbox" name="IO8" value="2">WW-LadePumpe<br> 
 <p>
    <input type="Submit" name="Loeschen" value="Loeschen"><br>
-   <input type="Submit" name="LED-abfragen" value="LED Status"><br>
+   <input type="Submit" name="IOabfragen" value="WerteStatus"><br>
    <br>
-   <input type="Submit" name="LED-setzen" value="LED setzen">
-   <input type="Submit" name="LED-loeschen" value="LED loeschen"><br>
+   <input type="Submit" name="IOsetzen" value="Port setzen">
+   <input type="Submit" name="IOloeschen" value="Port loeschen"><br>
 </form>
 
 <?php
